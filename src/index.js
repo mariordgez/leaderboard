6dd33f112434f8c8930b157a92fcbc4ec704061e
mariordgez <mariordgez@hotmail.com>
2021-09-02 15:15:22 -0700

    Add scores and scorelist

--- src/index.js
+++ src/index.js
@@ -3,3 +3,28 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+const lbName = document.querySelector('.lb-name');
+const lbScore = document.querySelector('.lb-score');
+
+const scoreAdd = document.querySelector('.score-add');
+const scoreRefresh = document.querySelector('.score-refresh');
+
+const addScore = (event) => {
+  event.preventDefault();
+  const newScore = new Task(lbName.value, lbScore.value);
+  Storage.saveTask(newScore);
+
+  UI.addToUI(newTask);
+  UI.checkTask();
+  UI.buttonTask();
+};
+
+const scoreRefresh = (event) => {
+  event.preventDefault();
+
+  window.location.reload();
+};
+
+scoreAdd.addEventListener('click', addScore);
+scoreRefresh.addEventListener('click', scoreRefresh);
