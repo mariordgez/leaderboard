1bfccea80fe807eafb23011bd6b980b512424689
mariordgez <mariordgez@hotmail.com>
2021-09-03 09:32:39 -0700

    Add input filter for scores

--- src/index.js
+++ src/index.js
@@ -15,13 +15,61 @@ const scoreRefresh = document.querySelector('.score-refresh');
 
 document.addEventListener(
   'DOMContentLoaded',
-  Display.displayList(Storage.getList().list),
+  Display.displayList(Storage.getList().list)
 );
+const getgame = () => {
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Marios game',
+      }),
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log('ERROR'));
+};
+
+const setInputFilter = (textbox, inputFilter) => {
+  [
+    'input',
+    'keydown',
+    'keyup',
+    'mousedown',
+    'mouseup',
+    'select',
+    'contextmenu',
+    'drop',
+  ].forEach(function (event) {
+    textbox.addEventListener(event, function () {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty('oldValue')) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = '';
+      }
+    });
+  });
+};
+
+setInputFilter(lbScore, function (value) {
+  return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+});
 
 const addScore = (event) => {
   event.preventDefault();
   const newScore = new Score(lbName.value, lbScore.value);
   Storage.saveScore(newScore);
+  getgame();
 
   Display.addToUI(newScore);
 };
