e1b250c48a87dc6e577e174f505f394855699ff0
mariordgez <mariordgez@hotmail.com>
2021-09-03 12:22:30 -0700

    Fix linter errors

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,7 @@
 export default class Storage {
   static getScores = async () => {
     const response = await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/MOPWIcjFvO9yMNxd0RRq/scores/'
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/MOPWIcjFvO9yMNxd0RRq/scores/',
     );
     const data = await response.json();
     const result = await data.result;
@@ -17,7 +17,7 @@ export default class Storage {
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      }
+      },
     );
     const data = response.json;
     const { result } = data;
